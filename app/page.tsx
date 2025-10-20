ed48a25c4783de30f00b3fd057b2cfa14e123128
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2025-10-20 09:42:20 +0500

    responsive fixed

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function app() {
   return (
-    <div className="bg-[#FFF9F9] w-full h-screen py-8">
+    <div className="bg-[#FFF9F9] w-full h-screen py-8 px-4">
       <div className="w-full flex flex-col justify-start items-center">
         <Image
           src={"/logo.svg"}
@@ -34,20 +34,23 @@ export default function app() {
         <h3 className="font-[700] text-xl md:text-[26px] text-[rgba(0,0,0,1)] leading-normal text-center mt-5">
           Be the First to Know When We Launch
         </h3>
-        <div className="flex gap-2.5 justify-start items-center mt-8">
+        <div className="w-full flex gap-2.5 justify-center items-center mt-8 flex-col md:flex-row">
           <Input
             placeholder="Enter your email address"
-            className="h-[45px] w-[350px] bg-white text-center"
+            className="h-[45px] w-full md:w-[350px] bg-white text-center"
             style={{ borderColor: "white" }}
           />
-          <Link href={"/blogs"}>
-            <Button className="" variant={"main_green_button"}>
+          <Link href={"/blogs"} className="w-full md:w-fit">
+            <Button className="w-full md:w-fit" variant={"main_green_button"}>
               Get Connected
             </Button>
           </Link>
         </div>
-        <Link href={"/blogs"}>
-          <Button className="mt-6" variant={"main_green_button"}>
+        <Link href={"/blogs"} className="w-full md:w-fit">
+          <Button
+            className="mt-6 w-full md:w-fit"
+            variant={"main_green_button"}
+          >
             Visit Blog Page
           </Button>
         </Link>
