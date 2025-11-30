19f385dc6f41b56336654ed269a700439855bbbc
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2025-12-01 00:12:59 +0500

    explore page fixed

--- app/(Protected)/explore/activity/activitiesCom.tsx
+++ app/(Protected)/explore/activity/activitiesCom.tsx
@@ -2,18 +2,18 @@
 import { useAppDispatch } from "@/lib/store/hooks";
 import { useMediaQuery } from "react-responsive";
 import { closeSidebar } from "@/lib/store/slices/sidebarSlice";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BoxProviderWithName } from "@/components/providers/BoxProviderWithName";
 import { TourAndActivityCard } from "@/components/TourAndActivityCard";
 import { ToursAndActivityWithVendor } from "@/lib/mongodb/models/ToursAndActivity";
-import axios from "axios";
 import { TourAndActivityCardSkeleton } from "@/components/Skeletons/TourAndActivityCardSkeleton";
 import { NoDataComponent } from "@/components/SmallComponents/NoDataComponent";
+import { ServerPaginationProvider } from "@/components/providers/PaginationProvider";
 
 const NoBookingsFound = () => {
   return (
     <div className="col-span-12 flex justify-center items-center">
-      <NoDataComponent text="No Activity Found" />
+      <NoDataComponent text="No Activities Found" />
     </div>
   );
 };
@@ -22,9 +22,9 @@ export default function ExploreActivities({
   filters = "",
   searchQuery,
 }: {
-  type: "both" | "Tour" | "Activity";
   filters?: string;
   searchQuery?: string;
+  type: "both" | "Tour" | "Activity";
 }) {
   const dispatch = useAppDispatch();
   const isMobile = useMediaQuery({ maxWidth: 1350 });
@@ -32,31 +32,6 @@ export default function ExploreActivities({
     if (isMobile) dispatch(closeSidebar());
   }, []);
   console.log("filters", filters);
-
-  const [activity, setActivity] = useState<ToursAndActivityWithVendor[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        setLoading(true);
-        let response = await axios.get(
-          `/api/toursAndActivity/getAll?category=Activity&search=${searchQuery}&limit=4&filters=${encodeURIComponent(
-            filters
-          )}`
-        );
-        console.log("response----", response);
-
-        if (response.data?.data) {
-          setActivity(response.data?.data);
-        }
-        setLoading(false);
-      } catch (error) {
-        console.log("err---", error);
-      }
-    };
-    getData();
-  }, [filters, searchQuery]);
 
   return (
     <BoxProviderWithName
@@ -66,19 +41,26 @@ export default function ExploreActivities({
       rightSideLink={type === "both" ? "/explore/activity" : undefined}
       rightSideLabel="View All Activities"
     >
-      <div className="w-full space-y-3 grid grid-cols-12 gap-3">
-        {loading ? (
-          [0, 1, 2, 3]?.map((item) => (
-            <TourAndActivityCardSkeleton key={item} />
-          ))
-        ) : activity && activity.length > 0 ? (
-          activity?.map((item, index) => (
-            <TourAndActivityCard item={item} key={index} />
-          ))
-        ) : (
-          <NoBookingsFound />
+      <ServerPaginationProvider<ToursAndActivityWithVendor>
+        apiEndpoint="/api/toursAndActivity/getAll" // Your API endpoint
+        queryParams={{
+          category: "Activity",
+          search: searchQuery,
+          filters: filters.includes("all") ? [] : filters,
+        }}
+        LoadingComponent={TourAndActivityCardSkeleton}
+        NoDataComponent={NoBookingsFound}
+        itemsPerPage={type === "both" ? 4 : 7}
+        fixedLimit={type === "both" ? true : false}
+      >
+        {(data, isLoading, refetch) => (
+          <div className="w-full space-y-3 grid grid-cols-12 gap-3">
+            {data?.map((item, index) => (
+              <TourAndActivityCard item={item} key={index} />
+            ))}
+          </div>
         )}
-      </div>
+      </ServerPaginationProvider>
     </BoxProviderWithName>
   );
 }
--- app/(Protected)/explore/tours/toursCom.tsx
+++ app/(Protected)/explore/tours/toursCom.tsx
@@ -2,13 +2,13 @@
 import { useAppDispatch } from "@/lib/store/hooks";
 import { useMediaQuery } from "react-responsive";
 import { closeSidebar } from "@/lib/store/slices/sidebarSlice";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BoxProviderWithName } from "@/components/providers/BoxProviderWithName";
 import { TourAndActivityCard } from "@/components/TourAndActivityCard";
 import { ToursAndActivityWithVendor } from "@/lib/mongodb/models/ToursAndActivity";
-import axios from "axios";
 import { TourAndActivityCardSkeleton } from "@/components/Skeletons/TourAndActivityCardSkeleton";
 import { NoDataComponent } from "@/components/SmallComponents/NoDataComponent";
+import { ServerPaginationProvider } from "@/components/providers/PaginationProvider";
 
 const NoBookingsFound = () => {
   return (
@@ -33,32 +33,6 @@ export default function ExploreTours({
   }, []);
   console.log("filters", filters);
 
-  const [tours, setTours] = useState<ToursAndActivityWithVendor[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const getData = async () => {
-      console.log("calling....");
-      try {
-        setLoading(true);
-        let response = await axios.get(
-          `/api/toursAndActivity/getAll?category=Tour&limit=4&search=${searchQuery}&filters=${encodeURIComponent(
-            filters
-          )}`
-        );
-        console.log("response----", response);
-
-        if (response.data?.data) {
-          setTours(response.data?.data);
-        }
-        setLoading(false);
-      } catch (error) {
-        console.log("err---", error);
-      }
-    };
-    getData();
-  }, [filters, searchQuery]);
-
   return (
     <BoxProviderWithName
       className="!py-0"
@@ -67,19 +41,26 @@ export default function ExploreTours({
       rightSideLink={type === "both" ? "/explore/tours" : undefined}
       rightSideLabel="View All Tours"
     >
-      <div className="w-full space-y-3 grid grid-cols-12 gap-3">
-        {loading ? (
-          [0, 1, 2, 3]?.map((item) => (
-            <TourAndActivityCardSkeleton key={item} />
-          ))
-        ) : tours && tours?.length > 0 ? (
-          tours?.map((item, index) => (
-            <TourAndActivityCard item={item} key={index} />
-          ))
-        ) : (
-          <NoBookingsFound />
+      <ServerPaginationProvider<ToursAndActivityWithVendor>
+        apiEndpoint="/api/toursAndActivity/getAll" // Your API endpoint
+        queryParams={{
+          category: "Tour",
+          search: searchQuery,
+          filters: filters.includes("all") ? [] : filters,
+        }}
+        LoadingComponent={TourAndActivityCardSkeleton}
+        NoDataComponent={NoBookingsFound}
+        itemsPerPage={type === "both" ? 4 : 7}
+        fixedLimit={type === "both" ? true : false}
+      >
+        {(data, isLoading, refetch) => (
+          <div className="w-full space-y-3 grid grid-cols-12 gap-3">
+            {data?.map((item, index) => (
+              <TourAndActivityCard item={item} key={index} />
+            ))}
+          </div>
         )}
-      </div>
+      </ServerPaginationProvider>
     </BoxProviderWithName>
   );
 }
