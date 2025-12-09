858fa1d86c4f119859552f960ecbe3d958e37991
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2025-12-09 14:06:55 +0500

    cron api and promotion image

--- app/(AuthLayout)/AuthLayoutProvider.tsx
+++ app/(AuthLayout)/AuthLayoutProvider.tsx
@@ -20,9 +20,19 @@ export function AuthLayoutProvider({
             : "auth-bg-image-2"
         } hidden lg:flex w-[50%] min-h-screen max-h-full`}
       ></div>
-      <div className="w-full lg:w-1/2 h-full">
+      <div
+        className={`w-full lg:w-1/2 ${
+          isVendor
+            ? "auth-bg-image-3"
+            : showImage1
+            ? "auth-bg-image-1"
+            : "auth-bg-image-2"
+        }`}
+      >
         {showImage1 && <SwitchRoles />}
-        <div className="flex justify-center items-start lg:items-center w-full h-[100vh] bg-white min-h-fit max-h-full">
+        <div
+          className={`flex justify-center items-start lg:items-center w-full h-[100vh] min-h-fit max-h-full `}
+        >
           <div className="flex-1 flex items-start lg:items-center justify-center py-6 lg:py-2 px-4 sm:px-6 lg:px-8 min-h-fit max-h-full">
             <div className="w-full max-w-md space-y-8 min-h-fit max-h-full">
               <main className="">{children}</main>
